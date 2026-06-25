08dceacd8863733ffe0e619c92b16bc4a6ea1b3b
Aneeshbhatia <bhatiaaneesh3@gmail.com>
2026-06-25 21:23:31 +0530

    .

--- key-shop/src/App.jsx
+++ key-shop/src/App.jsx
@@ -14,21 +14,25 @@ import './App.css';
 function App(){
  // const [cartCount, setCartCount]=useState(0);
  const [cartItems, setCartItems] = useState([]);
-
   function addToCart(product){
-   setCartItems((previousCartItems) =>{
+    if(!product){
+      return;
+    }
+    setCartItems((previousCartItems) =>{
     const productAlreadyInCart= previousCartItems.find((item)=>item.id==product.id);
 
       if(productAlreadyInCart){
+        
         return previousCartItems.map((item)=>{
-          item.id==product.id ? {...item, quantity: item.quantity +1 } : item;
+          return item.id==product.id ? {...item, quantity: item.quantity + 1 } : item;
         });
       } 
       return [...previousCartItems, {...product, quantity: 1}];
     })
   }
-
-  const cartCount = cartItems.reduce((total, item)=>total+item.quantity, 0);
+ 
+  const cartCount = cartItems.reduce((total, item)=> total + item.quantity, 0);
+  
 
   return (
     <BrowserRouter>
